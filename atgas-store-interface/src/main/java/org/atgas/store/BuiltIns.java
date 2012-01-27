1b1ca2d2e61c72eea4a0303aa08d03751b740c9f
Kenneth Leider <kennethleider@gmail.com>
2012-01-26 22:52:04 -0700

    Basic functionality exist

--- atgas-store-interface/src/main/java/org/atgas/store/BuiltIns.java
+++ atgas-store-interface/src/main/java/org/atgas/store/BuiltIns.java
@@ -5,29 +5,28 @@ import org.atgas.store.StandardUtils.Requirement;
 import org.atgas.store.StandardUtils.Type;
 
 public class BuiltIns {
-	public static final String BUILT_IN_SOURCE = "905a07a2-531e-4aa3-b90d-094f9f981050";
-	public static final String STANDARD_STANDARD = "848b326c-c80a-4bd3-baac-d371b1857385";
-	public static final String REPOSITORY_STANDARD = "2579d75f-fbd3-44e3-9474-7946b999b066";
-	public static final String ORGANIZATIONAL_SOURCE_STANDARD = "40ad9ea3-6443-47d3-a065-4af6373ff021";
-	
 
-	
-	public static Thing[] getBuiltInThings() {
-		return new Thing[] { createStandardStandard() };
-	}
-	
-	private static Thing createStandardStandard() {
-		Thing retval = new Thing(STANDARD_STANDARD, STANDARD_STANDARD, BUILT_IN_SOURCE);
-		retval.setProperty("name", "standard standard");
-		retval.setProperty("version", "1");
-		retval.setProperty("description", "The standard that describes standard things");
-		String fields = StandardUtils.joinFields(
-		       StandardUtils.formatField("name", Type.String, Requirement.Required, Multiplicity.Single),
-		       StandardUtils.formatField("version", Type.Integer, Requirement.Required, Multiplicity.Single),
-		       StandardUtils.formatField("description", Type.String, Requirement.Optional, Multiplicity.Single),
-		       StandardUtils.formatField("fields", Type.String, Requirement.Optional, Multiplicity.Multiple));
-		retval.setProperty("fields",fields);
-		
-		return retval;
-	}
+    public static final String BUILT_IN_SOURCE = "905a07a2-531e-4aa3-b90d-094f9f981050";
+    public static final String STANDARD_STANDARD = "848b326c-c80a-4bd3-baac-d371b1857385";
+    public static final String REPOSITORY_STANDARD = "2579d75f-fbd3-44e3-9474-7946b999b066";
+    public static final String ORGANIZATIONAL_SOURCE_STANDARD = "40ad9ea3-6443-47d3-a065-4af6373ff021";
+
+    public static Thing[] getBuiltInThings() {
+        return new Thing[]{createStandardStandard()};
+    }
+
+    private static Thing createStandardStandard() {
+        Thing retval = new Thing(STANDARD_STANDARD, STANDARD_STANDARD, BUILT_IN_SOURCE);
+        retval.setProperty("name", "standard standard");
+        retval.setProperty("version", "1");
+        retval.setProperty("description", "The standard that describes standard things");
+        String fields = StandardUtils.joinFields(
+                StandardUtils.formatField("name", Type.String, Requirement.Required, Multiplicity.Single),
+                StandardUtils.formatField("version", Type.Integer, Requirement.Required, Multiplicity.Single),
+                StandardUtils.formatField("description", Type.String, Requirement.Optional, Multiplicity.Single),
+                StandardUtils.formatField("fields", Type.String, Requirement.Optional, Multiplicity.Multiple));
+        retval.setProperty("fields", fields);
+
+        return retval;
+    }
 }
